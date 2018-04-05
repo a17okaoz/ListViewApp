395102f6c8e271ec102b7b1fcc86db24634d24eb
a17okaoz <a17okaoz@student.his.se>
2018-04-05 15:05:26 +0200

    Completed assignment

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -2,37 +2,62 @@ package org.brohede.marcus.listviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     // Let the static raw data that you use in your ListView be created here as a
     // member variable to the MainActivity class.
+    private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
+    private String[] mountainLocations = {"Alps","Alps","Alaska"};
+    private int[] mountainHeights ={4478,4808,6190};
+
+    protected ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // The onCreate method is run when the app is created.
-        // Before you can implement this you need to create the layout xml files that
-        // will hold/show your data created here. You need three create things:
-        // * my_listview - the ID to the actual layout element that is our ListView.
-        //                 This my_listview element is created in the activity_main.xml file
-        // * list_item_textview - This is a new layout xml file that holds the layout for an
-        //                        individual item in the ListView we are creating
-        // * my_item_textview - This is the ID to the actual TextView that will contain the text for
-        //                      an individual item in the ListView we are creating.
-        // Here you should enter your code that fills the ListView
-        // 1. Create an array
-        // 2. Create a List object with your array from step 1 as in-data
-        // 3. Create an ArrayAdapter object that connects
-        //    * list_item_textview
-        //    * my_item_textview
-        //    * List object created in step 2
-        // 4. Find the ListView layout element "my_listview" and create an object instance
-        // 5. Connect the ArrayAdapter from step 3 with ListView object created in step 4
-        // 6. Style the ListView items according to Material Design
-        //    See: https://material.io/guidelines/components/lists.html#lists-specs
-        //    Look for "singel line specs" for "text only" lists and modify the list_item_textview
-        //    accordingly
+        //Skapa en array
+        final String[] rawData = {"Okan", "Orkan", "Okian"};
+
+        //Skapa att list-objekt med din array som in data
+        List<String> listdata = new ArrayList<String>(Arrays.asList(rawData));
+
+        //Skapa en ArrayAdapter som kopplar samman
+        //List_item_textview, my_item_textview, och listan med rådata
+        adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,
+                R.id.my_item_listview, mountainNames);
+
+        //Koppla samman ListView med ArrayAdapter
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+
+        //Se att vi kan lägga till element i adaptern
+        //som automatiskt bir uppdaterat i ListView:n
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        mountainNames[position] + " " + mountainLocations[position] + " " + mountainHeights[position] ,
+                        Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        });
+
+
+
     }
 }
